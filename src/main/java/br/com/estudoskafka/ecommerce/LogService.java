27222b4f87cc02183076ee1613f43ee0b17f23ca
Filipi <lipi-valim@hotmail.com>
2025-05-29 17:32:03 -0300

    Migrando o LogService

--- src/main/java/br/com/estudoskafka/ecommerce/LogService.java
+++ src/main/java/br/com/estudoskafka/ecommerce/LogService.java
@@ -1,43 +1,34 @@
 package br.com.estudoskafka.ecommerce;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
 public class LogService {
-    public static void main(String[] args) throws InterruptedException {
 
-        var consumer = new KafkaConsumer<String, String>(properties());
-        consumer.subscribe(Pattern.compile("ECOMMERCE.*")); // A que escuta
-        while(true) {
-            var records = consumer.poll(Duration.ofMillis(1000));
-            if (records.isEmpty()) {
-                System.out.println("[RESGISTROS NÃO ENCONTRADOS]");
-                continue;
-            }
-            for (var record : records) {
-                System.out.println("-------------------------------------------");
-                System.out.println("[LOG]" + record.topic());
-                System.out.println(record.key());
-                System.out.println(record.value());
-                System.out.println(record.partition());
-                System.out.println(record.offset());
-            }
+
+    public static void main(String[] args) {
+        var logService = new LogService();
+        try (var service = new KafkaService(LogService.class.getSimpleName(),
+                Pattern.compile("ECOMMERCE.*"),
+                logService::parse)) {
+            service.run();
         }
-
     }
-    private static Properties properties(){
-        var properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getName());
 
-        return properties;
+    private void parse(ConsumerRecord<String, String> record) {
+        System.out.println("------------------------------------------");
+        System.out.println("LOG: " + record.topic());
+        System.out.println(record.key());
+        System.out.println(record.value());
+        System.out.println(record.partition());
+        System.out.println(record.offset());
     }
 }
