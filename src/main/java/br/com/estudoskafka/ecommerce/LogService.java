0c60ede49445843741fca8000c33b9f83c12faf9
Filipi <lipi-valim@hotmail.com>
2025-05-30 12:20:00 -0300

    Correção LOG

--- src/main/java/br/com/estudoskafka/ecommerce/LogService.java
+++ src/main/java/br/com/estudoskafka/ecommerce/LogService.java
@@ -8,6 +8,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.Collections;
+import java.util.Map;
 import java.util.Properties;
 import java.util.regex.Pattern;
 
@@ -17,7 +18,7 @@ public class LogService {
     public static void main(String[] args) {
         var logService = new LogService();
         try (var service = new KafkaService(LogService.class.getSimpleName(),
-                Pattern.compile("ECOMMERCE.*"),
+                String.valueOf(Pattern.compile("ECOMMERCE.*")),
                 logService::parse,
                 String.class,
                 Map.of(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName()))) {
