2cae8e6aa8f587cf2675420a5cd0d9fb97245999
Filipi <lipi-valim@hotmail.com>
2025-06-11 18:03:07 -0300

    Correção_e_email_semID

--- src/main/java/br/com/estudoskafka/ecommerce/CreateUserService.java
+++ src/main/java/br/com/estudoskafka/ecommerce/CreateUserService.java
@@ -7,6 +7,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class CreateUserService {
@@ -19,9 +20,14 @@ public class CreateUserService {
     CreateUserService() throws SQLException {
         String url = "jdbc:sqlite:target/users_database.db";
         connection = DriverManager.getConnection(url);
-        connection.createStatement().execute("create table Users (" +
-                        "uuid varchar(200) primary key," +
-                "email varchar(200))");
+        try {
+            connection.createStatement().execute("create table Users (" +
+                    "uuid varchar(200) primary key," +
+                    "email varchar(200))");
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
     }
 
 
@@ -58,16 +64,15 @@ public class CreateUserService {
     }
 
     private void insertNewUser(String email) throws SQLException {
-        var insert = connection.prepareStatement("insert into Users (uuid, email) + values (?,?)");
-        insert.setString(1, "uuid");
+        var insert = connection.prepareStatement("insert into Users (uuid, email) values (?, ?)");
+        insert.setString(1, UUID.randomUUID().toString());
         insert.setString(2, email);
         insert.execute();
         System.out.println("Usuário uuid e " + email + " adicionado");
     }
 
     private boolean isNewUser(String email) throws SQLException {
-        var exists = connection.prepareStatement("select uuid from Users " +
-                "where email = ? limit 1");
+        var exists = connection.prepareStatement("select uuid from users where email = ? limit 1");
         exists.setString(1, email);
         var results = exists.executeQuery();
         return !results.next();
